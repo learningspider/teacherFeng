d369536b2a141251a1b23d5be250d9b7e4c1052a
kevin <6168060@qq.com>
2017-12-28 16:23:11 +0800

    主页课程窗口offset+阴影完成

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,5 +1,19 @@
 $(document).ready(function(){
-    $('.course-item').click(function(){
-        alert('1');
+
+    $('.course-item').mouseenter(function(){
+        $(this).css("box-shadow","0 5px 5px #888888").offset(function(n,c){
+            newPos = new Object();
+            newPos.left=c.left-3;
+            newPos.top =c.top-3;
+            return newPos;
+        });
+    });
+    $('.course-item').mouseleave(function(){
+        $(this).css("box-shadow","").offset(function(n,c){
+            newPos = new Object();
+            newPos.left=c.left+3;
+            newPos.top =c.top+3;
+            return newPos;
+        });
     });
 });
